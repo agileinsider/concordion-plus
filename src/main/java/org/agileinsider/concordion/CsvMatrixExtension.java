4651b6e43abadd245fb011d9e461e182fdeb096f
agileinsider@gmail.com <agileinsider@gmail.com@f5fa0896-1729-cf1b-2d0a-608c1b9877bc>
2012-07-22 07:32:17 +0000

    Quick stab at working without JCVS on the path

--- src/main/java/org/agileinsider/concordion/CsvMatrixExtension.java
+++ src/main/java/org/agileinsider/concordion/CsvMatrixExtension.java
@@ -1,6 +1,9 @@
 package org.agileinsider.concordion;
 
 import org.agileinsider.concordion.command.CsvMatrixCommand;
+import org.concordion.api.CommandCall;
+import org.concordion.api.Evaluator;
+import org.concordion.api.ResultRecorder;
 import org.concordion.api.extension.ConcordionExtender;
 import org.concordion.api.extension.ConcordionExtension;
 import org.concordion.api.listener.AssertEqualsListener;
@@ -14,7 +17,7 @@ public class CsvMatrixExtension implements ConcordionExtension {
     private final AssertEqualsListener resultRenderer;
 
     public CsvMatrixExtension() {
-        this(new CsvMatrixCommand(), new AssertResultRenderer());
+        this(createCsvMatrixCommandSafely(), new AssertResultRenderer());
     }
 
     protected CsvMatrixExtension(CsvMatrixCommand csvMatrixCommand, AssertEqualsListener resultRenderer) {
@@ -28,4 +31,18 @@ public class CsvMatrixExtension implements ConcordionExtension {
         csvMatrixCommand.addAssertEqualsListener(resultRenderer);
     }
 
+    private static CsvMatrixCommand createCsvMatrixCommandSafely() {
+        try {
+            return new CsvMatrixCommand();
+        } catch (Throwable t) {
+            return new JCSVUnavailable();
+        }
+    }
+
+    private static class JCSVUnavailable extends CsvMatrixCommand {
+        @Override
+        public void verify(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
+            throw new IllegalStateException("Need to ensure that jcvs is available to use cvs-matrix.");
+        }
+    }
 }
